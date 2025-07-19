63b0cfdf06d13ae63ba4dcce3014557d51b066a3
sheida-shab <shabankary@gmail.com>
2025-07-19 13:58:22 +0100

    create episode card

--- script.js
+++ script.js
@@ -8,33 +8,36 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   const firstEpisode = episodeList[0];
   rootElem.textContent = "Popular TV Shows";
+  //create episode card
+  const episodeCard=document.createElement("div");
+
   //create show title
   const showTitle = document.createElement("h2");
   showTitle.textContent = `${firstEpisode.name}`;
-  rootElem.appendChild(showTitle);
+  episodeCard.appendChild(showTitle);
   //create show image
   const showImage = document.createElement("img");
   showImage.src = firstEpisode.image.medium;
-  rootElem.appendChild(showImage);
+  episodeCard.appendChild(showImage);
   //create show season and episode number
   const showSeasonEpisode = document.createElement("h3");
   console.log(String(firstEpisode.season).padStart(2, "0"));
   const showSeasonNumber = "S" + String(firstEpisode.season).padStart(2, "0");
   const showEpisodeNumber = "E" + String(firstEpisode.number).padStart(2, "0");
   showSeasonEpisode.textContent = showEpisodeNumber + showSeasonNumber;
-  rootElem.appendChild(showSeasonEpisode);
+  episodeCard.appendChild(showSeasonEpisode);
   //create show summary
   const showSummary = document.createElement("div");
-  showSummary.innerHTML = firstEpisode.summary;
-  rootElem.appendChild(showSummary);
+  showSummary.innerHTML = `<strong>The summary is : </strong>${firstEpisode.summary}`;
+  episodeCard.appendChild(showSummary);
   //create show original link
   const showLink = document.createElement("a");
-  showLink.textContent = "for mre information visit :";
+  showLink.textContent = `for mre information visit "tvmaze.com"`;
   showLink.target = "_blank";
   showLink.href = firstEpisode.url;
   console.log(showLink.textContent);
-  rootElem.appendChild(showLink);
-
+  episodeCard.appendChild(showLink);
+  rootElem.appendChild(episodeCard);
   console.log(episodeList);
 }
 
